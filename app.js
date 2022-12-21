93a48d89ec59a78a066b38cca9c5f37387add89a
makiweeb13 <mariamaekiskis13@gmail.com>
2022-12-21 13:19:00 +0800

    fixed minor bug and added responsiveness

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const initialize = () => {
     corners = [1, 3, 7, 9];
     tileNum = Object.values(numValue);
     winningTiles = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+    haveWon = false;
 }
 
 initialize();
